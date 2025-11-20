c54bad98789a1c7fac67bd5c1551e9e0aa8c7234
SHIAK AFRID <afridayan01@gmail.com>
2025-11-20 06:31:02 +0000

    Refactor cleanup script to use correct resource paths and namespace for Keycloak deployment

--- clean.sh
+++ clean.sh
@@ -5,22 +5,27 @@ set -e
 echo "Cleaning up Keycloak deployment..."
 
 echo "Deleting Keycloak resources..."
-kubectl delete -f 04-keycloak/keycloak-nodeport-service.yaml --ignore-not-found=true
-kubectl delete -f 04-keycloak/keycloak-statefulset.yaml --ignore-not-found=true
-kubectl delete -f 03-postgres/postgres-deployment.yaml --ignore-not-found=true
+kubectl delete -f keycloak/keycloak-nodeport-service.yaml --ignore-not-found=true
+kubectl delete -f keycloak/keycloak-statefulset.yaml --ignore-not-found=true
+kubectl delete -f postgres/postgres-storage-claim.yaml --ignore-not-found=true
+kubectl delete -f postgres/postgres-clusterip-service.yaml --ignore-not-found=true
+kubectl delete -f postgres/postgres-deployment.yaml --ignore-not-found=true
 
 echo "Waiting for pods to terminate..."
-kubectl wait --for=delete pod -l app=keycloak -n keycloak --timeout=120s || true
+kubectl wait --for=delete pod -l app=keycloak -n his-keycloak --timeout=120s || true
 
 echo "Deleting PVCs..."
-kubectl delete pvc -n keycloak --all
+kubectl delete pvc -n his-keycloak --all
 
 echo "Deleting storage resources..."
-kubectl delete -f 02-storage/persistent-volumes.yaml --ignore-not-found=true
-kubectl delete -f 02-storage/storageclass.yaml --ignore-not-found=true
+kubectl delete -f pvc/keycloak/keycloak-pv0.yaml --ignore-not-found=true
+kubectl delete -f pvc/keycloak/keycloak-pv1.yaml --ignore-not-found=true
+kubectl delete -f pvc/postgres/postgres-pv0.yaml --ignore-not-found=true
+kubectl delete -f pvc/persistent-volumes.yaml --ignore-not-found=true
 
 echo "Deleting secrets..."
-kubectl delete -f 01-secrets/secrets.yaml --ignore-not-found=true
+kubectl delete -f secrets/keycloak-secrets.yaml --ignore-not-found=true
+kubectl delete -f secrets/postgres-secrets.yaml --ignore-not-found=true
 
 echo "Deleting namespace..."
 kubectl delete -f 00-namespace/namespace.yaml --ignore-not-found=true
